5bbb6cb784b908c0a1fbe5177fca80e82e41fb09
wu <wuyuyue@cryptape.com>
2019-12-25 18:22:18 +0800

    chore: update muta-scan readme content, logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p></p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/nervosnetwork/muta"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Muta
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
